0217054f61fe03439b3af982b44abb3cadedacef
xjoe8 <youssef_bassem_8755@icloud.com>
2025-11-03 02:17:33 +0200

    Class Diagram Added

--- src/lab6/Main.java
+++
@@ -1,7 +0,0 @@
-package lab6;
-public class Main {
-    static public void main(String[] args){
-        JsonDatabaseManager file = new JsonDatabaseManager("users.json");
-        file.loadData();
-    }
-}
